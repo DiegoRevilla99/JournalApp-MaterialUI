da8eda79835fbadb7337eaf42e839adcb60087fb
Diego Revilla <string.diego.revilla@gmail.com>
2022-07-10 18:41:03 -0500

    Adding floating button

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,5 +1,7 @@
-import { Typography } from "@mui/material";
+import { AddOutlined } from "@mui/icons-material";
+import { IconButton, Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
+import { NoteView } from "../views/NoteView";
 import { NothingSelectedView } from "../views/NothingSelectedView";
 
 export const JournalPage = () => {
@@ -13,6 +15,21 @@ export const JournalPage = () => {
 			</Typography> */}
 
 			<NothingSelectedView />
+			{/* <NoteView /> */}
+
+			<IconButton
+				size="large"
+				sx={{
+					color: "white",
+					backgroundColor: "error.main",
+					":hover": { backgroundColor: "error.main", opacity: 0.9 },
+					position: "fixed",
+					right: 50,
+					bottom: 50,
+				}}
+			>
+				<AddOutlined sx={{ fontSize: 30 }} />
+			</IconButton>
 		</JournalLayout>
 	);
 };
