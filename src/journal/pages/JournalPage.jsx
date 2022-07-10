d6bc10b4bdd13c66d5584a9076b81d616d6b802e
Diego Revilla <string.diego.revilla@gmail.com>
2022-07-10 18:09:45 -0500

    Adding fist View

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,15 +1,18 @@
 import { Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
+import { NothingSelectedView } from "../views/NothingSelectedView";
 
 export const JournalPage = () => {
 	return (
 		<JournalLayout>
-			<Typography>
+			{/* <Typography>
 				Ad amet ut qui incididunt fugiat pariatur occaecat consectetur officia.
 				Reprehenderit do aliquip ipsum in minim consectetur reprehenderit non fugiat non.
 				Officia irure dolore magna est elit adipisicing nisi officia exercitation sit
 				adipisicing mollit proident ex. Id mollit quis qui sit pariatur ex.
-			</Typography>
+			</Typography> */}
+
+			<NothingSelectedView />
 		</JournalLayout>
 	);
 };
