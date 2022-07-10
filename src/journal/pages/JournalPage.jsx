0fb4cb4f9643bfb6b68667c64aa4ba52688c65de
Diego Revilla <string.diego.revilla@gmail.com>
2022-07-09 21:56:25 -0500

    Adding Navbar, Sidebar, and Jornal

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,15 @@
-import { MailOutline } from "@mui/icons-material";
 import { Typography } from "@mui/material";
+import { JournalLayout } from "../layout/JournalLayout";
 
 export const JournalPage = () => {
 	return (
-		<>
-			<Typography variant="h1">JournalPage</Typography>
-			<MailOutline />
-		</>
+		<JournalLayout>
+			<Typography>
+				Ad amet ut qui incididunt fugiat pariatur occaecat consectetur officia.
+				Reprehenderit do aliquip ipsum in minim consectetur reprehenderit non fugiat non.
+				Officia irure dolore magna est elit adipisicing nisi officia exercitation sit
+				adipisicing mollit proident ex. Id mollit quis qui sit pariatur ex.
+			</Typography>
+		</JournalLayout>
 	);
 };
